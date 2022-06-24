7b5f57a8116b7edb64145075f10ecd3e4fb33868
joyjsmun <joy.js.mun@gmail.com>
2022-06-24 17:51:54 +0900

    ADD: tailwind css prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import type { NextPage } from "next"
 const Home: NextPage = () => {
   return (
    <div className="bg-red-500">
-     <h1 className="text-black">It works</h1>
+     <h1 className="text-green-500">It works</h1>
    </div>
   )
 }
